936f1ea11764218b4f5bd1f633dcdfc42200b6c0
Waihon Yew <yewwaihon@gmail.com>
2019-03-04 21:16:42 +0800

    Order the ideas in reverse chronological order

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
   class IdeasController < ApplicationController
     def index
-      @ideas = Idea.all
+      @ideas = Idea.order("created_at DESC")
       render json: @ideas
     end
 
