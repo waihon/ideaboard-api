cb5ce2d3574ed3de04e948742946c65d41332c09
Waihon Yew <yewwaihon@gmail.com>
2019-03-04 18:28:00 +0800

    API endpoint for creating a new idea

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -4,5 +4,16 @@ module Api::V1
       @ideas = Idea.all
       render json: @ideas
     end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+
+    private
+
+      def idea_params
+        params.require(:idea).permit(:title, :body)
+      end
   end
 end
